60a7d37f8779ff3371089d7357cdb4a1dcae141e
Hadenix <vsegda_online_007@bk.ru>
2021-11-29 21:00:04 +0300

    m40_n6h8: Initial TWRP device tree
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

---
+++ Android.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),m40_n6h8)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_m40_n6h8.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_m40_n6h8-user \
+    omni_m40_n6h8-userdebug \
+    omni_m40_n6h8-eng
